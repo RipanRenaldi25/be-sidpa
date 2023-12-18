ece5efff9bbff2c8f8cc8340b598f12c9d6914e5
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2023-12-18 23:16:42 +0700

    feat(seed): change the way seed invoked

--- src/index.ts
+++ src/index.ts
@@ -10,24 +10,24 @@ import UserController from "./Interfaces/Controllers/user.controller";
 // midlewares
 import { authMidleware } from "./Interfaces/Midlewares/authMidleware";
 
-app.get('/', (req, res) => res.send('ok2'))
-app.use('/auth', authRoutes);
-app.use('/user', authMidleware, userRoutes)
-app.use('/users', authMidleware, UserController.getUsers);
-app.use('/requests', authMidleware, requestRouter)
 
-userRepositoryConcrete.seed({
-    name: 'admin',
-    nik: '123',
-    phoneNumber: '08123123122',
-    roleId: '2',
-    username: 'admin1',
-    password: process.env.ADM_PW!
-})
-.then(() => {
-    app.listen(+process.env.PORT_APP!, () => console.log(`Application Running on PORT ${process.env.PORT_APP}`));
-})
-.catch(err => {
-    console.log('admin already exists')
-    app.listen(+process.env.PORT_APP!, () => console.log(`Application Running on PORT ${process.env.PORT_APP}`));
-});
+const init = async () => {
+    app.get('/', (req, res) => res.send('ok2'))
+    app.use('/auth', authRoutes);
+    app.use('/user', authMidleware, userRoutes)
+    app.use('/users', authMidleware, UserController.getUsers);
+    app.use('/requests', authMidleware, requestRouter)
+
+    await userRepositoryConcrete.seed({
+        name: 'admin',
+        nik: '123',
+        phoneNumber: '08123123122',
+        roleId: '2',
+        username: 'admin1',
+        password: process.env.ADM_PW!
+    });
+
+    app.listen(+process.env.PORT_APP!, () => console.log(`Application Running on Port ${process.env.PORT_APP!}`));
+};
+
+init();
